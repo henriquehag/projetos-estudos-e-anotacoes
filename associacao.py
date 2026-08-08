4573ea57a7110e742172ad989e07084fabf2aa42
henriquehag <henrique2006.hagemann@gmail.com>
2026-08-08 12:46:09 -0300

    adição de arquivos

--- associacao.py
+++
@@ -1,8 +0,0 @@
-frase = "Eu adoro programar em Python"
-
-print("Python" in frase) # Saída: True
-print("python" in frase) # Saída: False (o 'p' minúsculo faz diferença)
-print("Java" in frase)   # Saída: False
-
-print("Python" not in frase) # Saída: False
-print("Java" not in frase)   # Saída: True
--- comparacao.py
+++
@@ -1,6 +0,0 @@
-x == x #igualdade
-x != x #diferença
-x > x #maior
-x < x #menor
-x >= x #maior ou igual
-x <= x #menor ou igual
